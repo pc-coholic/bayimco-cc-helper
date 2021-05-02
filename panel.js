c33e8cda38c465a33e347576bcef9cf194813d6f
Martin Gross <martin@pc-coholic.de>
2021-05-02 18:24:32 +0200

    Check with proper regex' if we are on the right pages/requests

--- panel.js
+++ panel.js
@@ -2,15 +2,15 @@ function handleRequestFinished(request) {
     request.getContent((body) => {
         if (request.request && request.request.url) {
             chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-                let url = tabs[0].url;
-                if (!url.startsWith('https://intern.impfzentren.bayern/callcenter/citizen/overview/')) {
+                let taburl = tabs[0].url;
+                // https://impfzentren.bayern/citizen/overview/AD99E6C9-7169-44DB-820C-E4F36D0A6F9E
+                // https://intern.impfzentren.bayern/callcenter/citizen/overview/AD99E6C9-7169-44DB-820C-E4F36D0A6F9E
+                if (!/^https:\/\/(intern.|uat.|intern-uat.)?impfzentren.bayern\/(callcenter\/)?citizen\/overview\/[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/.test(taburl)) {
                     cleanup();
                 } else {
-                    let split = request.request.url.split('/')
-                    if (
-                        request.request.url.match('https://intern.impfzentren.bayern/api/v1/call-center/citizens/') && 
-                        split[split.length -1].length == 36
-                    ) {
+                    // https://impfzentren.bayern/api/v1/citizens/AD99E6C9-7169-44DB-820C-E4F36D0A6F9E
+                    // https://impfzentren.bayern/api/v1/call-center/citizens/AD99E6C9-7169-44DB-820C-E4F36D0A6F9E
+                    if (/^https:\/\/(intern.|uat.|intern-uat.)?impfzentren.bayern\/api\/v1\/(call-center\/)?citizens\/[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/.test(request.request.url)) {
                         processPersonData(body);
                     }
                 }
